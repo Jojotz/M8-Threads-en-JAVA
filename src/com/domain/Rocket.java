5e53a554f31956547d08f3bf4c8717331c560166
Joan Coll <67835708+Jojotz@users.noreply.github.com>
2020-11-17 13:16:02 +0100

    Add files via upload

--- src/com/domain/Rocket.java
+++ src/com/domain/Rocket.java
@@ -7,16 +7,19 @@ public class Rocket {
 
 	private String code;
 	private List <Thruster> thrusters = new ArrayList <Thruster> ();
-		
-	public Rocket(String code, List <Thruster> thrusters) throws Exception {
+	private int maxTotPowerR;
+	
+	public Rocket(String code, List <Thruster> thrusters, int maxTotPowerR) throws Exception {
 		
 		if (code.equals(""))
 			throw new Exception();
 					
 		this.code = code;
 		this.thrusters = thrusters;
+		this.maxTotPowerR = maxTotPowerR;
 	}
-			
+	
+	//Método que devuelve String de potencia máxima de cada propulsor en un cohete 		
 	public String thrustMaxPowers () {
 		
 		String powerThrusters = "";
@@ -27,12 +30,41 @@ public class Rocket {
 		}
 		return powerThrusters;
 	}
+	
+	//Método que devuelve int de la potencia total actual de todos los propulsores de todos los cohetes. 
+	public int getTotalCurrentPower () {
 		
+		int totalPower = 0;
+		
+			for (Thruster a: thrusters) {
+			
+				totalPower += a.getCurrentPower();
+			}		
+			return totalPower;
+	}
+	
+	//Método que devuelve String la potencia total actual de un cohete con todos sus propulsores. 
+	public String getRocketCurrentPower () {
+		
+		String individualPower = "";
+		
+		for (Thruster a: thrusters) {
+		
+			individualPower += "[" + a.getCurrentPower() + "/" + a.getMaxPower() + "]" ;
+		}		
+		return individualPower;
+		
+	}
+	
+	public int getMaxTotPowerR() {
+		return maxTotPowerR;
+	}
+
 	public String getCode() {
 		return code;
 	}
 
 	public List <Thruster> getThrusters() {
 		return thrusters;
-	}
+	}	
 }
--- src/com/domain/Thruster.java
+++ src/com/domain/Thruster.java
@@ -1,19 +1,85 @@
 package com.domain;
 
-import java.util.ArrayList;
-import java.util.List;
-
-public class Thruster {
+public class Thruster implements Runnable {
 	
-	private int maxPower;      		//Max power of a thruster
+	private int maxPower;      		//Maximum power of a thruster
 	private int currentPower; 		//Current power of a thruster
-		
-	public Thruster (int currentPower, int maxPower) {
+	private int targetPower;		//Total target power
+	private Rocket rocket;			
+	private int totCurrentPower;    //Total current power of all thrusters
+	
+	public Thruster (int currentPower, int maxPower, int targetPower, Rocket rocket) {
 		
 		this.currentPower = currentPower;
 		this.maxPower = maxPower;
-	
+		this.targetPower = targetPower;
+		this.rocket = rocket;
 	} 
+
+	public void run () {	
+		
+		totCurrentPower = getRocket().getTotalCurrentPower();
+				
+		if (getTargetPower() > totCurrentPower) {
+			
+			speedUp();
+		
+		} else if (getTargetPower() < totCurrentPower) {
+				
+			brake();	
+		} 					
+	}
+
+	public synchronized void speedUp () {
+			
+		while (currentPower < maxPower && (getTargetPower() > getRocket().getTotalCurrentPower())) {
+			
+			currentPower++;				
+			System.out.println(toString());
+			try {
+				Thread.sleep(300);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+				return;
+			}				
+		} 
+	}
+	
+	public synchronized void brake () {
+		
+		while (currentPower > 0 && (getTargetPower() < getRocket().getTotalCurrentPower())) {
+			
+			currentPower--;
+			System.out.println(toString());
+			try {
+				Thread.sleep(300);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+				return;
+			}
+		} 
+	}	
+	
+	@Override
+	public String toString () {
+		
+		StringBuilder output = new StringBuilder();
+		output.append(getRocket().getCode());
+		output.append(": ");
+		output.append("P:");
+		output.append(getRocket().getRocketCurrentPower() +"\n");
+		
+		if (getTargetPower() == getRocket().getTotalCurrentPower()) {
+			
+			output.append(" ---> El coet: " + getRocket().getCode() + " ha arribat a la potència objectiu." + "\n");
+		
+		} else if ((getTargetPower() > getRocket().getTotalCurrentPower()) && ( getTargetPower() > getRocket().getMaxTotPowerR()) && (getRocket().getTotalCurrentPower() ==  getRocket().getMaxTotPowerR())) {
+			
+			output.append(" ---> El coet: " + getRocket().getCode() + " ha arribat a la potència màxima, i no podrá assolir l'objectiu." + "\n");
+		}
+		
+		return output.toString();
+	}	
 		
 	public int getMaxPower() {
 		return maxPower;
@@ -23,8 +89,19 @@ public class Thruster {
 		return currentPower;		
 	}
 
-	public void setCurrentPower(int currentPower) {
-		this.currentPower = currentPower;
+	public int getTargetPower() {
+		return targetPower;
 	}
 
+	public void setTargetPower(int targetPower) {
+		this.targetPower = targetPower;
+	}
+	
+	public Rocket getRocket() {
+		return rocket;
+	}
+
+	public void setRocket(Rocket rocket) {
+		this.rocket = rocket;
+	}
 }
